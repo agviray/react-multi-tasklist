f8d5df1c36ee78f2bfcf5caed5841261e927524b
Allan Viray <allang.v.dev@gmail.com>
2023-04-08 18:08:37 -0500

    Apply transition on vertical scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyledHeader } from './styles/StyledHeader.styled';
 
 const Header = () => {
+  const [scrollYPos, setScrollYPos] = useState(0);
+  const [isTransitionApplied, setIsTransitionApplied] = useState(false);
+  useEffect(() => {
+    const handleVerticalScroll = () => {
+      if (typeof window !== 'undefined') {
+        setScrollYPos(window.scrollY);
+      }
+    };
+
+    window.addEventListener('scroll', handleVerticalScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleVerticalScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scrollYPos > 0) {
+      if (isTransitionApplied) {
+        return;
+      }
+      setIsTransitionApplied(true);
+    }
+
+    if (scrollYPos === 0) {
+      if (isTransitionApplied) {
+        setIsTransitionApplied(false);
+      }
+    }
+  }, [scrollYPos]);
+
   return (
-    <StyledHeader>
+    <StyledHeader isTransitionApplied={isTransitionApplied}>
       <h1>Multi-Tasklist</h1>
     </StyledHeader>
   );
--- src/components/styles/StyledHeader.styled.js
+++ src/components/styles/StyledHeader.styled.js
@@ -9,6 +9,18 @@ export const StyledHeader = styled.header`
   background-color: white;
   z-index: 10;
 
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    opacity: ${({ isTransitionApplied }) => (isTransitionApplied ? '1' : '0')};
+    box-shadow: 0px 5px 9px 0px rgba(0, 0, 0, 0.16);
+    transition: opacity 0.3s ease;
+  }
+
   h1 {
     text-align: center;
     font-size: 30px;
