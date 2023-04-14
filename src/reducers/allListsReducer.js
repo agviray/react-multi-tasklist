a9ed7f8fc0e0fecf976e3551f79e2719e3649749
Allan Viray <allang.v.dev@gmail.com>
2023-04-14 17:33:01 -0500

    Add logic for each switch case

--- src/reducers/allListsReducer.js
+++ src/reducers/allListsReducer.js
@@ -1,16 +1,21 @@
 import { types } from '../actions/types';
 
 const INITIAL_STATE = [];
+
 const allListsReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case types.INITIAL_LISTS_LOADED:
-      return 'hi';
+      return [...action.payload];
     case types.NEW_LIST_SAVED:
-      return 'hi';
+      return [action.payload, ...state];
     case types.UPDATED_LIST_SAVED:
-      return 'hi';
+      const editedList = action.payload;
+      const editedListId = editedList.id;
+      let savedLists = [...state];
+      savedLists.filter((list) => list.id !== editedListId);
+      return [editedList, ...savedLists];
     case types.LIST_DELETED:
-      return 'hi';
+      return [...state].filter((list) => list.id !== action.payload);
 
     default:
       return state;
